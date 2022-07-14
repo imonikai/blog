6c2c2d792ffbb9cac836505fcc574a1e03c9a74e
Imonikai <108214765+Imonikai@users.noreply.github.com>
2022-07-14 23:49:53 +0900

    最新以外の記事削除時にリダイレクトされないバグを修正

--- admin/select.js
+++ admin/select.js
@@ -1,30 +1,32 @@
 'use strict';
 
 window.addEventListener('load', ()=> {
-    const deleteBtn = document.querySelector('.delete-btn'); 
+    const deleteBtn = document.querySelectorAll('.delete-btn'); 
 
-    deleteBtn.addEventListener('click', (e) => {
-    
-        e.preventDefault();
-    
-        const url = new URL(deleteBtn.href);
-    
-        fetch(url)
-        .then(response => {
-            return response.json();
-        }).then(data => {
-            console.log(data)
-            if(data.result != 'OK')
-            {
-                throw new Error('操作が完了できませんでした');
-            }
-            else
-            {
-                window.location.href = './index.php';
-            } 
-        }).catch(err => {
-            alert(err);
-            console.log(err);
-        })
-    });
-}, false);
+    deleteBtn.forEach( button => {
+        button.addEventListener('click', (e) => {
+        
+            e.preventDefault();
+        
+            const url = new URL(button.href);
+        
+            fetch(url)
+            .then(response => {
+                return response.json();
+            }).then(data => {
+                console.log(data)
+                if(data.result != 'OK')
+                {
+                    throw new Error('操作が完了できませんでした');
+                }
+                else
+                {
+                    window.location.href = './index.php';
+                } 
+            }).catch(err => {
+                alert(err);
+                console.log(err);
+            })
+        });
+    })
+});
